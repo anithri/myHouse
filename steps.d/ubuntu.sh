dafd687cf747528f68ab9b77d7faca53764354e6
Scott M Parrish <anithri@gmail.com>
2019-04-26 13:48:52 -0600

    add wifi instructions

--- steps.d/ubuntu.sh
+++ steps.d/ubuntu.sh
@@ -8,5 +8,7 @@ function installAptPackages() {
                            bison libssl-dev libyaml-dev libreadline6-dev \
                            zlib1g-dev libncurses5-dev libffi-dev libgdbm5 \
                            libgdbm-dev postgresql-10 pgadmin4 zsh i3 direnv \
-                           autojump
+                           autojump ubuntu-drivers-common mesa-utils \
+                           mesa-utils-extra gnupg numlockx xautolock scrot \
+                           xorg xserver-xorg wget unzip wpasupplicant
 }
